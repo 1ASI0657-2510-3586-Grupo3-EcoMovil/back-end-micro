60414692ad03829ee2daae2a43b4a3f911b1ba9a
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2025-06-09 14:19:24 -0500

    feat: finalizado

--- create-users-db.sql
+++
@@ -1,7 +0,0 @@
-
-# Users Service Database Configuration
-CREATE DATABASE IF NOT EXISTS ecomovil_users_db;
-CREATE USER IF NOT EXISTS 'users_user'@'%' IDENTIFIED BY 'users_password';
-GRANT ALL PRIVILEGES ON ecomovil_users_db.* TO 'users_user'@'%';
-FLUSH PRIVILEGES;
-
